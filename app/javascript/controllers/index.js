5f9228d16fdffca438a91611278f107effa01b89
Italo Bandeira <italobpl94@gmail.com>
2023-06-06 15:35:10 -0300

    Turning the index card into a link.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,7 @@
 import { application } from "./application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-eagerLoadControllersFrom("controllers", application)
+// eagerLoadControllersFrom("./controllers", application)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
