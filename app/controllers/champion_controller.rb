502223186f7982131e2f54d55b6c2a378d594ce1
Italo Bandeira <italobpl94@gmail.com>
2023-08-02 15:16:14 -0300

    Change colors in icons footer

--- app/controllers/champion_controller.rb
+++ app/controllers/champion_controller.rb
@@ -2,14 +2,14 @@ class ChampionController < ApplicationController
   before_action :set_champions
 
   def index
-    @all_champions     = @champions_service.all_champions
-    @image_champions   = @champions_service.image_champions
+    @all_champions = @champions_service.all_champions
+    @image_champions = @champions_service.image_champions
   end
 
   def show
-    @one_champion        = @champions_service.one_champion(params[:id])
-    @splash_champion     = @champions_service.splash_champion(params[:id])
-    @image_spell         = @champions_service.image_spell
+    @one_champion = @champions_service.one_champion(params[:id])
+    @splash_champion = @champions_service.splash_champion(params[:id])
+    @image_spell = @champions_service.image_spell
     @image_passive_spell = @champions_service.image_passive_spell
   end
 
